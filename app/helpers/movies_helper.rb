0536c72780c1aa20dcf799f45e29b46b3580251b
Padi Ignacio <marcrendlignacio@gmail.com>
2012-06-14 22:56:25 +0800

    Add "hilite" class to movie header if movies are sorted by that header

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,9 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  # gives a "hilite" class if the page described is the current page
+  def highlight_if_clicked(page_info={})
+    "hilite" if current_page?(page_info)
+  end
 end
