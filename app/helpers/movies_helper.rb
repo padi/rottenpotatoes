02ededa68ea0328a1d50cc84a5ebb7739a25551d
Padi Ignacio <marcrendlignacio@gmail.com>
2012-06-16 11:44:27 +0800

    Refactor: Add highlighted_header_link_to to refactor view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -5,11 +5,15 @@ module MoviesHelper
   end
 
   # gives a "hilite" class if the page described is the current page
-  def highlight_if_clicked(page_info={})
-    "hilite" if current_page?(page_info)
+  def highlight_if_clicked(url)
+    current_page?(url) ? "hilite" : ""
   end
 
   def is_checked? rating
     params[:ratings] && params[:ratings].include?(rating)
+  end
+
+  def highlighted_header_link_to body, url, html_options = {}
+    content_tag(:th, link_to(body, url, html_options), :class => highlight_if_clicked(url))
   end
 end
